121e56bd1a979713ba862f61042c1e38ed8d9813
Maksim Aleksyuk <maleksjuk@yandex.ru>
2024-07-07 14:13:37 +0300

    clean ip_filter.cpp; try add workflow

--- 02/ip_filter.cpp
+++ 02/ip_filter.cpp
@@ -7,29 +7,21 @@
 
 using ip_addres = std::vector<std::string>;
 
-// ("",  '.') -> [""]
-// ("11", '.') -> ["11"]
-// ("..", '.') -> ["", "", ""]
-// ("11.", '.') -> ["11", ""]
-// (".11", '.') -> ["", "11"]
-// ("11.22", '.') -> ["11", "22"]
-std::vector<std::string> split(const std::string &str, char d)
+std::vector<std::string> split(const std::string &str, char separator)
 {
-    std::vector<std::string> r;
+    std::vector<std::string> string_list;
 
     std::string::size_type start = 0;
-    std::string::size_type stop = str.find_first_of(d);
+    std::string::size_type stop = str.find_first_of(separator);
     while(stop != std::string::npos)
     {
-        r.push_back(str.substr(start, stop - start));
-
+        string_list.push_back(str.substr(start, stop - start));
         start = stop + 1;
-        stop = str.find_first_of(d, start);
+        stop = str.find_first_of(separator, start);
     }
+    string_list.push_back(str.substr(start));
 
-    r.push_back(str.substr(start));
-
-    return r;
+    return string_list;
 }
 
 std::ostream &operator<<(std::ostream &out, const ip_addres &addr)
@@ -44,12 +36,11 @@ std::ostream &operator<<(std::ostream &out, const ip_addres &addr)
 }
 
 
-int main(/*int argc, char const *argv[]*/)
+int main()
 {
-
     try
     {
-        std::vector<ip_addres > ip_pool;
+        std::vector<ip_addres> ip_pool;
 
         for (std::string line; std::getline(std::cin, line);)
         {
@@ -57,8 +48,8 @@ int main(/*int argc, char const *argv[]*/)
             ip_pool.push_back(split(v.at(0), '.'));
         }
 
-        // TODO reverse lexicographically sort
-
+        // reverse lexicographically sort
+        
         std::sort(ip_pool.begin(), ip_pool.end(),
             [](const ip_addres &left, const ip_addres &right){
                 for (size_t i = 0; i < left.size(); i++) {
@@ -71,24 +62,7 @@ int main(/*int argc, char const *argv[]*/)
         for (auto ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
             std::cout << *ip << std::endl;
 
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
         // TODO filter by first byte and output
-        // ip = filter(1)
-
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // std::cout << "-------------------------------------" << std::endl;
 
         std::for_each(ip_pool.begin(), ip_pool.end(),
             [](const ip_addres &addr){
@@ -97,14 +71,6 @@ int main(/*int argc, char const *argv[]*/)
             });
 
         // TODO filter by first and second bytes and output
-        // ip = filter(46, 70)
-
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-
-        // std::cout << "-------------------------------------" << std::endl;
         
         std::for_each(ip_pool.begin(), ip_pool.end(),
             [](const ip_addres &addr){
@@ -115,43 +81,6 @@ int main(/*int argc, char const *argv[]*/)
 
         // TODO filter by any byte and output
         // ip = filter_any(46)
-
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
-
-        // std::cout << "-------------------------------------" << std::endl;
         
         std::for_each(ip_pool.begin(), ip_pool.end(),
             [](const ip_addres &addr){
